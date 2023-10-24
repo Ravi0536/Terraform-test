036ff5db1e1ea81692b7d81761e88ae4c2ac6641
root <root@L-H5CG0178WZ2.PRCINS.NET>
2023-10-24 09:00:11 -0400

    small change

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # Define your outputs.tf configurations here
 output "ec2_instance_id" {
-  description = "AWS instance id"
+  description = "AWS instance id output"
   value       = "${aws_instance.example.id}"
 }
