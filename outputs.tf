e5c7295eb4cd87981c87617c02773f3dad702df1
root <root@L-H5CG0178WZ2.PRCINS.NET>
2023-10-24 10:29:08 -0400

    webhook test

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # Define your outputs.tf configurations here
 output "ec2_instance_id" {
-  description = "AWS instance id output"
+  description = "AWS instance id"
   value       = "${aws_instance.example.id}"
 }
